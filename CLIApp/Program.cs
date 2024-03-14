c7db84e673be6eb1ced9271a6218a235ab1ded85
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-14 22:35:51 +0500

    Add project files.

---
+++ CLIApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage : SimpleCalculator.exe <number1> <number2>");
+                return;
+            }
+
+            if (!double.TryParse(args[0], out double num1) || !double.TryParse(args[1], out double num2))
+            {
+                Console.WriteLine("Invalid input. Please provide a valid numbers.");
+                return;
+            }
+
+
+            double sum = num1 + num2;
+            Console.WriteLine($"Sum of {num1} and {num2} is : {sum}");
+        }
+    }
+}
